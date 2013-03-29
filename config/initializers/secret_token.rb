0708d7ff1b62427f930c18b36f3f001fe7efc888
David Crowther <crowtherdr@familysearch.org>
2013-03-29 00:34:08 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerOsxDemo::Application.config.secret_token = 'e7e833c73ea4ec574c00fc832bc59296d815f81c04ee9b02fe7305043dd69f74131bbd8ee08f0e4c68b1a5a2439a422f9a7d86b296e95b51670f3c80b291ad11'
